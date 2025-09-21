2f365ba0b4f96cb71f082ab184966ec28b878693
tsuchiya-yu2 <yuji2tsuchiya@gmail.com>
2025-09-21 19:53:26 +0900

    WIP

--- articles/views.py
+++ articles/views.py
@@ -18,6 +18,18 @@ class ArticleCreateView(LoginRequiredMixin, CreateView):
     template_name = 'articles/create.html'
     success_url = reverse_lazy('articles:list')
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'page_title': '記事を投稿する',
+            'page_description': '新しい記事を投稿してみましょう。',
+            'cancel_url': reverse_lazy('articles:list'),
+            'cancel_text': 'キャンセル',
+            'submit_text': '投稿する',
+            'submit_icon_path': 'M12 6v6m0 0v6m0-6h6m-6 0H6'
+        })
+        return context
+
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
@@ -38,8 +50,20 @@ class ArticleUpdateView(LoginRequiredMixin, UpdateView):
     def dispatch(self, request, *args, **kwargs):
         article = self.get_object()
         if request.user != article.author:
-            return redirect('articles:detail', pk=article.pk)
+            return redirect('/')
         return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'page_title': '記事を編集する',
+            'page_description': '記事の内容を編集してください。',
+            'cancel_url': reverse_lazy('articles:detail', kwargs={'pk': self.object.pk}),
+            'cancel_text': 'キャンセル',
+            'submit_text': '更新する',
+            'submit_icon_path': 'M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3-3m0 0l-3 3m3-3v12'
+        })
+        return context
 
     def get_success_url(self):
         return reverse_lazy('articles:detail', kwargs={'pk': self.object.pk})
